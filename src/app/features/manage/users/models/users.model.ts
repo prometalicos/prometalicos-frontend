8ab776b2a63ebd90bbb21d1136cc1fcecbed4bab
Adriana Vargas <avargas@latinia.com>
2022-05-18 22:27:01 -0500

    http service

---
+++ src/app/features/manage/users/models/users.model.ts
@@ -0,0 +1,7 @@
+export interface User {
+    usuario_id: any;
+    rol_id: number;
+    nombre_completo: any;
+    direccion: any;
+    estado: any;
+}
---
+++ src/app/features/manage/users/services/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/users.model';
+import { HttpService } from 'src/app/core/services/http.service';
+
+@Injectable()
+export class UserService {
+
+  constructor(protected http: HttpService) { }
+
+  public consultar() {
+    const baseUrl = environment.endpoint + '/usuarios';
+    return this.http.doGet<User[]>(baseUrl);
+  }
+
+  public consultarId(id: string) {
+    return this.http.doGet<User>(`${environment.endpoint}/usuarios/` + id, this.http.optsName('consultar agentes'));
+  }
+
+  public guardar(usuario: User) {
+      return this.http.doPost<User, boolean>(`${environment.endpoint}/usuarios`, usuario);
+  }
+
+  public editar(usuario: User, id: string) {
+      return this.http.doPut<User, boolean>(`${environment.endpoint}/usuarios/${id}`, usuario);
+  }
+
+  public eliminar(id: string) {
+    return this.http.doDelete<boolean>(`${environment.endpoint}/usuarios/${id}`,
+      this.http.optsName('eliminar usuario'));
+  }
+}
